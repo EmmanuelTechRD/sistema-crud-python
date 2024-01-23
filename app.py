23a120cf147d0c9961180dca4d041e8f47854a10
Emmanuel Soto <emmanueltechrd@gmail.com>
2024-01-23 12:09:18 -0400

    se agregó formulario y se probó conexión enviando entradas a la base de datos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -15,13 +15,40 @@ mysql.init_app(app)
 @app.route('/')
 def index():
 
-    sql = "INSERT INTO `empleados` (`id`, `nombre`, `telefono`, `correo`, `cedula`, `fecha_de_nacimiento`) VALUES (NULL, 'Emmanuel', '1-829-555-1111', 'enicolassotop@gmail.com', '001-2345678-9', '01 de Enero del 1234');"
+    sql = "SELECT * FROM `empleados`;"
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
+
+    empleados = cursor.fetchall()
+    print(empleados)
+
     conn.commit()
 
-    return render_template('empleados/index.html')
+    return render_template('empleados/index.html', empleados = empleados)
+
+@app.route('/crear')
+def create():
+
+    return render_template('empleados/create.html')
+
+@app.route('/almacenar', methods=['POST'])
+def storage():
+
+    _nombre = request.form['txtNombre']
+    _telefono = request.form['txtTelefono']
+    _correo = request.form['txtCorreo']
+    _cedula = request.form['txtCedula']
+    _fecha_de_nacimiento = request.form['txtFechadeNacimiento']
+
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `telefono`, `correo`, `cedula`, `fecha_de_nacimiento`) VALUES (NULL, %s, %s, %s, %s, %s);"
+    datos = (_nombre, _telefono, _correo, _cedula, _fecha_de_nacimiento)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    return render_template('empleados/create.html')
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
